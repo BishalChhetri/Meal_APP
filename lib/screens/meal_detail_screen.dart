576ac9bbedb4ce0848378605e3cf0ed6260f8be6
Bishal KC <kcbisal1@gmail.com>
2021-04-10 08:27:40 +0530

    Meal App

---
+++ lib/screens/meal_detail_screen.dart
@@ -0,0 +1,95 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import '../dummy_data.dart';
+
+class MealDetailScreen extends StatelessWidget {
+  static const routeName = '/MealDetail';
+
+  Widget buildSectionTitle(BuildContext context, String title) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        vertical: 10,
+      ),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.title,
+      ),
+    );
+  }
+
+  Widget buildContainer(Widget child) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      height: 150,
+      width: 300,
+      child: child,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final mealID = ModalRoute.of(context).settings.arguments as String;
+    final selectMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealID);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${selectMeal.title}'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(selectMeal.imageUrl, fit: BoxFit.cover),
+            ),
+            buildSectionTitle(context, 'Ingredients'),
+            buildContainer(
+              ListView.builder(
+                  itemBuilder: (ctx, index) => Card(
+                        color: Theme.of(ctx).accentColor,
+                        child: Padding(
+                          padding:
+                              EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                          child: Text(
+                            selectMeal.ingredients[index],
+                          ),
+                        ),
+                      ),
+                  itemCount: selectMeal.ingredients.length),
+            ),
+            buildSectionTitle(context, 'Steps'),
+            buildContainer(
+              ListView.builder(
+                itemBuilder: (ctx, index) => Column(
+                  children: <Widget>[
+                    ListTile(
+                      leading: CircleAvatar(
+                        child: Text('# ${index + 1}'),
+                      ),
+                      title: Text(
+                        selectMeal.steps[index],
+                      ),
+                    ),
+                    Divider(),
+                  ],
+                ),
+                itemCount: selectMeal.steps.length,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.delete),
+        onPressed: () {},
+      ),
+    );
+  }
+}
